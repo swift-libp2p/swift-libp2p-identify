f765c17960d98c305db1d80f5251af9a2197685c
Brandon <32753167+btoms20@users.noreply.github.com>
2023-07-25 08:51:38 -0700

    Added mplex and noise packages as test dependencies for integration tests.

--- Package.swift
+++ Package.swift
@@ -17,7 +17,9 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/swift-libp2p/swift-libp2p.git", .upToNextMajor(from: "0.1.0"))
+        .package(url: "https://github.com/swift-libp2p/swift-libp2p.git", .upToNextMajor(from: "0.1.0")),
+        .package(url: "https://github.com/swift-libp2p/swift-libp2p-mplex.git", .upToNextMajor(from: "0.1.0")),
+        .package(url: "https://github.com/swift-libp2p/swift-libp2p-noise.git", .upToNextMajor(from: "0.1.0"))
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -32,6 +34,10 @@ let package = Package(
             ]),
         .testTarget(
             name: "LibP2PIdentifyTests",
-            dependencies: ["LibP2PIdentify"]),
+            dependencies: [
+                "LibP2PIdentify",
+                .product(name: "LibP2PNoise", package: "swift-libp2p-noise"),
+                .product(name: "LibP2PMPLEX", package: "swift-libp2p-mplex")
+            ]),
     ]
 )
