08f32a929fbb43804443a22d87cb3c4e81f9f457
Brandon <btoms.20@gmail.com>
2026-08-19 15:34:02 -0700

    formatting

--- Package.swift
+++ Package.swift
@@ -30,7 +30,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        
+
         .package(url: "https://github.com/swift-libp2p/swift-libp2p.git", .upToNextMinor(from: "0.3.4")),
 
         // Test Dependencies
